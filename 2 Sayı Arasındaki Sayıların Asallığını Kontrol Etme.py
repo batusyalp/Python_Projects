ab34858f98c9851e7810bfc88b1b12878c631090
dogukansurucu <surucudogukann3@gmail.com>
2022-09-29 00:45:39 +0300

    Add files via upload

---
+++ 2 Sayı Arasındaki Sayıların Asallığını Kontrol Etme.py
@@ -0,0 +1,18 @@
+# 2 sayı arasındaki sayıların asal olup olmadığını gösteren algoritma
+sayi1 = int(input('number 1:'))
+sayi2 = int(input('number 2:'))
+
+
+def asal(number1, number2):
+    for number in range(number1, number2 + 1):
+        if number > 1:
+            ''' üstteki satırdaki koddaki olay şu: iki sayı arasındaki asalları bulacağımızdan ve de asallar pozif olduğundan dolayı sayılar 1 den büyük olmak zorunda'''
+            for i in range(2, number):
+                if number % i == 0:
+                    break
+            else:
+                print(f"{number} asal sayıdır.")
+        else:
+            print(f"{number} asal değildir.")
+
+asal(sayi1, sayi2)
